07a0b34c07dd1385d6cc5daab19cee0ff2968b97
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-09-17 23:44:12 -0300

    final 2 implementations

--- EasyCookApp/EasyCookApp/Controllers/NewRecipeViewControllerThree.swift
+++ EasyCookApp/EasyCookApp/Controllers/NewRecipeViewControllerThree.swift
@@ -4,9 +4,50 @@
 //
 //  Created by Gabriel Ferreira de Carvalho on 17/09/21.
 //
-
+import Combine
 import UIKit
 
 class NewRecipeViewControllerThree: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    @IBAction func sendRecipe(_ sender: Any) {
+        Recipe.shared.preparationMethod = textView.text
+        
+        var request = URLRequest(url: URL(string: "http://localhost:3000/addRecipe")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = try! JSONEncoder().encode(Recipe.shared)
+        
+        URLSession.shared
+            .dataTaskPublisher(for: request)
+            .map(\.response)
+            .map { $0 as? HTTPURLResponse }
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: printResult,
+                  receiveValue: printResponse)
+            .store(in: &cancellables)
+    }
+    
+    private func printResult(_ result: Subscribers.Completion<URLSession.DataTaskPublisher.Failure>) {
+        switch result {
+        case .failure(let error):
+            print(error)
+        case .finished:
+            print("sucess")
+        }
+
+    }
+    
+    private func printResponse(_ response: HTTPURLResponse?) {
+        guard let response = response else {
+            print("fudeu")
+            return
+        }
+        dismiss(animated: true, completion: nil)
+        print("Response: \(response.statusCode)")
+    }
     
 }
